3ccf369874776169ca4fd62e71c251af5a589aea
kennansalisbury <kennansalisbury@gmail.com>
2020-06-23 10:23:36 -0400

    stubbed routes

---
+++ controllers/dogs.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const fs = require('fs')
+
+
+//Index: GET - /dogs
+
+//Show/Details: GET - /dogs/:id
+
+//Add: POST - /dogs
+
+//Edit: PUT - /dogs/:id
+
+//Destroy: DELETE - /dogs/:id
+
+
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,25 @@
+// ----------  DECLARATIONS  --------------------
+const express = require('express')
+const layouts = require('express-ejs-layouts')
+const app = express()
+const methodOverride = require('method-override')
+
+app.set('view engine', 'ejs')
+app.use('/', express.static('static'))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+/*------------- ROUTES/CONTROLLERS --------------*/
+//Controllers:
+app.use('/cats', require('./controllers/cats'))
+app.use('/dogs', require('./controllers/dogs'))
+
+//Home: GET - /
+app.get('/', (req,res) => {
+    // res.render('home')
+    res.send('home')
+})
+
+
+// ----------  PORT  --------------------
+app.listen(3000)
